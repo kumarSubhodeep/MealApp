b379c6a5fc585606343075e79de3a178877079e5
Subhodeep Kumar <kumarsubhodeep4sb@gmail.com>
2021-06-30 09:19:31 +0530

    30 june part 2 - 1 hr complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:meal_app/screens/categories.dart';
 import 'package:meal_app/screens/category_meals.dart';
+import 'package:meal_app/screens/filters.dart';
 import 'package:meal_app/screens/meal_detail.dart';
+import 'package:meal_app/screens/tabs.dart';
 
 void main() => runApp(MyApp());
 
@@ -23,18 +24,20 @@ class MyApp extends StatelessWidget {
               body2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
               // ignore: deprecated_member_use
               title: TextStyle(
-                color: Colors.white,
+                // color: Colors.white,
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
                 fontFamily: 'RobotoCondensed',
               ),
             ),
       ),
-      home: Categories(),
+      home: Tabs(),
+      // initialRoute: '/',
       routes: {
-        // '/': (ctx) => Categories(),
+        // '/': (ctx) => Tabs(),
         MealDetails.routeName: (context) => MealDetails(),
         CategoryMeals.routeName: (context) => CategoryMeals(),
+        Filters.routeName: (context) => Filters(),
       },
       // onGenerateRoute: (settings) {
       //   print(settings.arguments);
--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -5,27 +5,22 @@ import 'package:meal_app/widgets/category_item.dart';
 class Categories extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('DeliMeal'),
-      ),
-      body: GridView(
-        padding: const EdgeInsets.all(25),
-        children: DUMMY_CATEGORIES
-            .map(
-              (e) => CategoryItem(
-                e.id,
-                e.title,
-                e.color,
-              ),
-            )
-            .toList(),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
+    return GridView(
+      padding: const EdgeInsets.all(25),
+      children: DUMMY_CATEGORIES
+          .map(
+            (e) => CategoryItem(
+              e.id,
+              e.title,
+              e.color,
+            ),
+          )
+          .toList(),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
       ),
     );
   }
